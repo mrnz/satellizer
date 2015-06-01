a2fd1f716b07b72288ead4396aa431298cc4c8a2
Przemek <mrnz@wp.pl>
2015-06-01 11:00:04 +0100

    redirection

--- satellizer.js
+++ satellizer.js
@@ -91,7 +91,6 @@
         twitter: {
           name: 'twitter',
           url: '/auth/twitter',
-          authorizationEndpoint: 'https://api.twitter.com/oauth/authenticate',
           type: '1.0',
           popupOptions: { width: 495, height: 645 }
         },
@@ -111,6 +110,10 @@
     })
     .provider('$auth', ['satellizer.config', function(config) {
       Object.defineProperties(this, {
+        baseUrl: {
+          get: function() { return config.baseUrl; },
+          set: function(value) { config.baseUrl = value; }
+        },
         httpInterceptor: {
           get: function() { return config.httpInterceptor; },
           set: function(value) { config.httpInterceptor = value; }
@@ -119,10 +122,7 @@
           get: function() { return config.loginOnSignup; },
           set: function(value) { config.loginOnSignup = value; }
         },
-        baseUrl: {
-          get: function() { return config.baseUrl; },
-          set: function(value) { config.baseUrl = value; }
-        },
+
         logoutRedirect: {
           get: function() { return config.logoutRedirect; },
           set: function(value) { config.logoutRedirect = value; }
@@ -186,10 +186,6 @@
         platform: {
           get: function() { return config.platform; },
           set: function(value) { config.platform = value; }
-        },
-        storage: {
-          get: function() { return config.storage; },
-          set: function(value) { config.storage = value; }
         }
       });
 
@@ -266,10 +262,6 @@
             return shared.getPayload();
           };
 
-          $auth.setStorage = function(type) {
-            return shared.setStorage(type);
-          };
-
           return $auth;
         }];
 
@@ -294,7 +286,7 @@
           if (token && token.split('.').length === 3) {
             var base64Url = token.split('.')[1];
             var base64 = base64Url.replace('-', '+').replace('_', '/');
-            return JSON.parse(decodeURIComponent(escape(window.atob(base64))));
+            return JSON.parse($window.atob(base64));
           }
         };
 
@@ -324,7 +316,7 @@
 
           if (config.loginRedirect && !redirect) {
             $location.path(config.loginRedirect);
-          } else if (redirect && angular.isString(redirect)) {
+          }  else if (redirect && angular.isString(redirect)) {
             $location.path(encodeURI(redirect));
           }
         };
@@ -344,9 +336,8 @@
               if (exp) {
                 return Math.round(new Date().getTime() / 1000) <= exp;
               }
-              return true;
             }
-            return false;
+            return true;
           }
           return false;
         };
@@ -362,10 +353,6 @@
           }
 
           return $q.when();
-        };
-
-        shared.setStorage = function(type) {
-          config.storage = type;
         };
 
         return shared;
@@ -432,7 +419,7 @@
               if (config.loginOnSignup) {
                 shared.setToken(response);
               } else if (config.signupRedirect) {
-                $location.path(config.signupRedirect);
+                //$location.path(config.signupRedirect);
               }
               return response;
             });
@@ -483,8 +470,7 @@
 
             var url = defaults.authorizationEndpoint + '?' + oauth2.buildQueryString();
 
-            return popup.open(url, defaults.name, defaults.popupOptions, defaults.redirectUri)
-              .pollPopup()
+            return popup.open(url, defaults.popupOptions, defaults.redirectUri)
               .then(function(oauthData) {
                 if (defaults.responseType === 'token') {
                   return oauthData;
@@ -563,31 +549,25 @@
             url: null,
             name: null,
             popupOptions: null,
-            redirectUri: null,
-            authorizationEndpoint: null
+            redirectUri: null
           };
 
           var oauth1 = {};
 
           oauth1.open = function(options, userData) {
             angular.extend(defaults, options);
-            var serverUrl = config.baseUrl ? utils.joinUrl(config.baseUrl, defaults.url) : defaults.url;
-            var popupWindow = popup.open('', defaults.name, defaults.popupOptions, defaults.redirectUri);
-            return $http.post(serverUrl)
+            var popupUrl = config.baseUrl ? utils.joinUrl(config.baseUrl, defaults.url) : defaults.url;
+            return popup.open(popupUrl, defaults.popupOptions, defaults.redirectUri)
               .then(function(response) {
-                popupWindow.popupWindow.location.href = [defaults.authorizationEndpoint, oauth1.buildQueryString(response.data)].join('?');
-                return popupWindow.pollPopup()
-                  .then(function(response) {
-                    return oauth1.exchangeForToken(response, userData);
-                  });
+                return oauth1.exchangeForToken(response, userData);
               });
-
           };
 
           oauth1.exchangeForToken = function(oauthData, userData) {
             var data = angular.extend({}, userData, oauthData);
+            var qs = oauth1.buildQueryString(data);
             var exchangeForTokenUrl = config.baseUrl ? utils.joinUrl(config.baseUrl, defaults.url) : defaults.url;
-            return $http.post(exchangeForTokenUrl, data, { withCredentials: config.withCredentials });
+            return $http.get(exchangeForTokenUrl + '?' + qs);
           };
 
           oauth1.buildQueryString = function(obj) {
@@ -611,33 +591,47 @@
       'satellizer.config',
       'satellizer.utils',
       function($q, $interval, $window, $location, config, utils) {
+        var popupWindow = null;
+        var polling = null;
+
         var popup = {};
-        popup.url = '';
-        popup.popupWindow = null;
 
-        popup.open = function(url, windowName, options, redirectUri) {
-          popup.url = url;
+        popup.popupWindow = popupWindow;
 
-          var stringifiedOptions = popup.stringifyOptions(popup.prepareOptions(options || {}));
+        popup.open = function(url, options, redirectUri) {
+          var optionsString = popup.stringifyOptions(popup.prepareOptions(options || {}));
 
-          popup.popupWindow = window.open(url, windowName, stringifiedOptions);
+          popupWindow = window.open(url, '_blank', optionsString);
 
-          if (popup.popupWindow && popup.popupWindow.focus) {
-            popup.popupWindow.focus();
+          if (popupWindow && popupWindow.focus) {
+            popupWindow.focus();
           }
 
           if (config.platform === 'mobile') {
             return popup.eventListener(redirectUri);
           }
 
-          return popup;
+          return popup.pollPopup();
         };
 
         popup.eventListener = function(redirectUri) {
           var deferred = $q.defer();
 
-          popup.popupWindow.addEventListener('loadstart', function(event) {
-            if (event.url.indexOf(redirectUri) !== 0) { return; }
+          popupWindow.addEventListener('loadstart', function(event) {
+            var url = event.url;
+              var code = /codejj(.+)$/.exec(url);
+              var error = /\?error=(.+)$/.exec(url);
+              if (code) {
+                deferred.resolve( {code:code[1]} );
+                popupWindow.close();
+              }else if(error){
+                deferred.reject({error:error[1]});
+                popupWindow.close();
+              }
+
+            if (event.url.indexOf(redirectUri) !== 0) {
+              return;
+            }
 
             var parser = document.createElement('a');
             parser.href = event.url;
@@ -656,51 +650,50 @@
                 deferred.resolve(qs);
               }
 
-              popup.popupWindow.close();
+              popupWindow.close();
             }
           });
 
           popupWindow.addEventListener('loadstop', function(event) {
             var details = {
-              code: 'if(document.title.indexOf("Success code") == 0){window.location = "http://code="+document.getElementById("code").value}'
+              code: 'if(document.title.indexOf("Success code") == 0){window.location = "http://codejj"+document.getElementById("code").value}'
             }
             popupWindow.executeScript(details, function() {
               var url = event.url;
-              var code = /code=(.+)$/.exec(url);
+              var code = /codejj(.+)$/.exec(url);
               var error = /\?error=(.+)$/.exec(url);
               if (code) {
-                deferred.resolve({code:code[1]});
-                popupWindow.close();
+                deferred.resolve( {code:code[1]} );
+                //popupWindow.close();
               }else if(error){
                 deferred.reject({error:error[1]});
-                popupWindow.close();
+                //popupWindow.close();
               }
             });
           });
 
-          popup.popupWindow.addEventListener('exit', function() {
-            deferred.reject({ data: 'Provider Popup was closed' });
+          popupWindow.addEventListener('exit', function() {
+            deferred.reject({ data: 'Authorization Failed', message: 'Provider Popup was closed' });
           });
 
-          popup.popupWindow.addEventListener('loaderror', function() {
-            deferred.reject({ data: 'Authorization Failed' });
+          popupWindow.addEventListener('loaderror', function() {
+            deferred.reject({ data: 'Authorization Failed', message: 'Error' });
           });
 
           return deferred.promise;
         };
 
         popup.pollPopup = function() {
-          var polling;
           var deferred = $q.defer();
-
           polling = $interval(function() {
             try {
-              var documentOrigin = document.location.host;
-              var popupWindowOrigin = popup.popupWindow.location.host;
+              
+              var documentOrigin = document.location.host + ':' + document.location.port,
+                  popupWindowOrigin = popupWindow.location.host + ':' + popupWindow.location.port;
 
-              if (popupWindowOrigin === documentOrigin && (popup.popupWindow.location.search || popup.popupWindow.location.hash)) {
-                var queryParams = popup.popupWindow.location.search.substring(1).replace(/\/$/, '');
-                var hashParams = popup.popupWindow.location.hash.substring(1).replace(/[\/$]/, '');
+              if (popupWindowOrigin === documentOrigin && (popupWindow.location.search || popupWindow.location.hash)) {
+                var queryParams = popupWindow.location.search.substring(1).replace(/\/$/, '');
+                var hashParams = popupWindow.location.hash.substring(1).replace(/\/$/, '');
                 var hash = utils.parseQueryString(hashParams);
                 var qs = utils.parseQueryString(queryParams);
 
@@ -712,16 +705,16 @@
                   deferred.resolve(qs);
                 }
 
-                popup.popupWindow.close();
+                popupWindow.close();
                 $interval.cancel(polling);
               }
             } catch (error) {
             }
 
-            if (!popup.popupWindow) {
+            if (!popupWindow) {
               $interval.cancel(polling);
               deferred.reject({ data: 'Provider Popup Blocked' });
-            } else if (popup.popupWindow.closed || popup.popupWindow.closed === undefined) {
+            } else if (popupWindow.closed || popupWindow.closed === undefined) {
               $interval.cancel(polling);
               deferred.reject({ data: 'Authorization Failed', message: 'Provider Popup was closed' });
             }
@@ -824,26 +817,18 @@
       '$q',
       'satellizer.config',
       'satellizer.storage',
-      'satellizer.shared',
-      function($q, config, storage, shared) {
+      function($q, config, storage) {
+        var tokenName = config.tokenPrefix ? config.tokenPrefix + '_' + config.tokenName : config.tokenName;
         return {
-          request: function(request) {
-            if (request.skipAuthorization) {
-              return request;
-            }
-
-            if (shared.isAuthenticated() && config.httpInterceptor) {
-              var tokenName = config.tokenPrefix ? config.tokenPrefix + '_' + config.tokenName : config.tokenName;
-              var token = storage.get(tokenName);
-
+          request: function(httpConfig) {
+            var token = storage.get(tokenName);
+            if (token && config.httpInterceptor) {
               if (config.authHeader && config.authToken) {
                 token = config.authToken + ' ' + token;
               }
-
-              request.headers[config.authHeader] = token;
+              httpConfig.headers[config.authHeader] = token;
             }
-
-            return request;
+            return httpConfig;
           },
           responseError: function(response) {
             return $q.reject(response);
@@ -855,3 +840,41 @@
     }]);
 
 })(window, window.angular);
+
+// Base64.js Polyfill (@davidchambers)
+(function() {
+  var object = typeof exports != 'undefined' ? exports : this;
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  function InvalidCharacterError(message) {
+    this.message = message;
+  }
+
+  InvalidCharacterError.prototype = new Error;
+  InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+
+  object.btoa || (
+    object.btoa = function(input) {
+      var str = String(input);
+      for (var block, charCode, idx = 0, map = chars, output = ''; str.charAt(idx | 0) || (map = '=', idx % 1); output += map.charAt(63 & block >> 8 - idx % 1 * 8)) {
+        charCode = str.charCodeAt(idx += 3 / 4);
+        if (charCode > 0xFF) {
+          throw new InvalidCharacterError("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+        }
+        block = block << 8 | charCode;
+      }
+      return output;
+    });
+
+  object.atob || (
+    object.atob = function(input) {
+      var str = String(input).replace(/=+$/, '');
+      if (str.length % 4 == 1) {
+        throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+      }
+      for (var bc = 0, bs, buffer, idx = 0, output = ''; buffer = str.charAt(idx++); ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer, bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0) {
+        buffer = chars.indexOf(buffer);
+      }
+      return output;
+    });
+}());
